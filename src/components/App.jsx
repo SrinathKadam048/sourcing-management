b6f8acb0e1b78f129ede39bffa853fdc3da91a2f
SrinathKadam048 <srinathkadam04@gmail.com>
2023-07-29 12:22:34 +0530

    v1 of Header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Login from "./LoginPage"
 import Dashboard from "./Dashboard";
+import Header from "./Header";
 
 function App() {
     return (
         <div>
-            <Login />
-            <Dashboard />  
+            <Header />
+            <Dashboard />
         </div>
     );
 }
