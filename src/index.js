05549766262fec7eb9d45c34e0a5f49eeda2b9d9
SrinathKadam048 <srinathkadam04@gmail.com>
2023-07-31 23:21:03 +0530

    commenting out custom.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './custom.css';
+// import './custom.css';
 ReactDOM.render(
   <BrowserRouter>
     <App />
