4b54a721b86ec90cd88941c9643cd122e2b1b1bf
SrinathKadam048 <srinathkadam04@gmail.com>
2023-08-07 10:00:53 +0530

    Form Validation added for itemID. Pending POST

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const inventoryController = require('../controllers/inventoryController');
 
 router.get('/api/inventory', inventoryController.getAllItems);
+router.get(`/api/inventory/checkItem/:itemId`, inventoryController.checkItem);
+router.post('/api/inventory/addItem', inventoryController.addItem);
 
 module.exports = router;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Dashboard from "./Dashboard";
 import Header from "./Header";
 import { Route, Routes } from 'react-router-dom';
 import Inventory from "./Inventory";
+import AddItem from "./AddItem"
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <Route path="" element={<Login />}/>
             <Route path="/Dashboard" element={<Dashboard />} />
             <Route path="/Inventory" element={<Inventory />} />
+            <Route path="/add-item" element={<AddItem />}/>
         </Routes>
         </div>
     );
