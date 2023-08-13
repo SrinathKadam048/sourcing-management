049bcb48982e5ae5a13eb0b6b30a26e6b4ab18b3
SrinathKadam048 <srinathkadam04@gmail.com>
2023-08-13 17:27:31 +0530

    port setting according to Render

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Connect to MongoDB (Replace 'mongodb://localhost:27017/inventory_db' with your database connection string)
 mongoose.connect(process.env.DATABASE, {
